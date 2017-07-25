4176b6ee93929a47f6f2eff69ba292449b2cdf27
Janci Krajnak <jan.krajnak@email.cz>
2017-07-25 18:47:31 +0200

    Added scraper functions

--- index.py
+++ index.py
@@ -1,5 +1,43 @@
-import bs4 as bs
-from urllib.request import urlopen as req
+from bs4 import BeautifulSoup as bs
+from urllib.request import Request, urlopen
 
-url = 'http://www.maltapark.com/listings.aspx?category=14'
 
+def create_file():
+    filename = 'someFile.csv'
+    f = open(filename, 'w')
+    headers = 'product, link\n'
+    f.write(headers)
+    return f
+
+
+def append_page_items(page, file):
+    items = page.find_all(class_='item')
+    for item in items:
+        link = item.find('a').get('href')
+        try:
+            title = item.find(class_='title').get_text().replace('WANTED: ', '').replace(',', '|')
+        except AttributeError:
+            title = None
+        finally:
+            if title is not None:
+                file.write(title + ',' + link + '\n')
+
+
+def get_html(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    conn = urlopen(req)
+    page_html = conn.read()
+    conn.close()
+    return bs(page_html, 'html.parser')
+
+# get page
+page = get_html('http://www.maltapark.com/listings.aspx?category=14&lt=wa')
+
+# create new file
+file = create_file()
+
+# parse items and write them
+append_page_items(page, file)
+
+# close
+file.close()
